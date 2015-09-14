56e98c22342d23950ba8400efb7d51b53ad56a0a
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-09-14 14:18:31 -0400

    Quick Changes

---
+++ src/ProgrammingExercise1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richj0985
+ */
+public class ProgrammingExercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // make city for robot to live in
+        City kw = new City();
+    }
+}
