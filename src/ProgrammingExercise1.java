9a91b15a19e4b9865170def396a8bc52a254c2c9
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-09-14 14:24:03 -0400

    Quick Changes

--- src/ProgrammingExercise1.java
+++ src/ProgrammingExercise1.java
@@ -1,3 +1,8 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -15,5 +20,7 @@ public class ProgrammingExercise1 {
     public static void main(String[] args) {
         // make city for robot to live in
         City kw = new City();
+        // make robot to live in city
+        Robot karel = new Robot (kw, 1, 1, Direction.EAST);
     }
 }
