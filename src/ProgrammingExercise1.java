8cbca915b98de3ea68907cd6e7b0acd39f2d860c
Jon <Jon@Jon-HP>
2015-09-14 19:02:13 -0400

    Home Change

--- src/ProgrammingExercise1.java
+++ src/ProgrammingExercise1.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 
 /*
  * To change this template, choose Tools | Templates
@@ -20,7 +21,34 @@ public class ProgrammingExercise1 {
     public static void main(String[] args) {
         // make city for robot to live in
         City kw = new City();
+        
         // make robot to live in city
         Robot karel = new Robot (kw, 1, 1, Direction.EAST);
+        
+        //make things for robot to pick up
+        new Thing(kw, 1, 2);
+        new Thing(kw, 1, 3);
+        new Thing(kw, 1, 4);
+        new Thing(kw, 1, 5);
+        new Thing(kw, 1, 6);
+        new Thing(kw, 1, 7);
+        new Thing(kw, 1, 8);
+        new Thing(kw, 1, 9);
+        new Thing(kw, 1, 10);
+        new Thing(kw, 1, 11);
+        
+        //make robot walk forward and pick up things
+        while(karel.frontIsClear() == true)
+        {
+            if(karel.canPickThing() && karel.countThingsInBackpack() < 7)
+            {
+                karel.pickThing();
+                karel.move();
+            }else
+            {
+                karel.move();
+            }
+        }
+        
     }
 }
