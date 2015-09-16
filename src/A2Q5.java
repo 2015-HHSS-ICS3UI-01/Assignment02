f1340c244d09ca9251305051368d924525b6d724
Peter <Peter@Katrina>
2015-09-16 16:30:11 -0400

    A2 Q1,Q2, Q3, Q4  done     Q5 started

---
+++ src/A2Q5.java
@@ -0,0 +1,32 @@
+
+import becker.robots.City;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Peter
+ */
+public class A2Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //make city
+        City kw = new City();
+        
+        //make walls
+        
+        //make things
+        
+        //make robots
+        
+        
+    }
+    
+}
