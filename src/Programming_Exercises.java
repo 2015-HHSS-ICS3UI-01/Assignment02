6a1c03c494ec337a4826e027d968950096d4bf86
Matt <Matt@AllegaeonV2>
2015-09-16 14:24:36 -0400

    added Programming_Exercises, started, added city (kw) and robot (matt, facing east, @ 1,1,)

---
+++ src/Programming_Exercises.java
@@ -0,0 +1,31 @@
+
+import becker.robots.Wall;
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Matt
+ */
+public class Programming_Exercises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        City kw = new City();
+        Robot matt = new Robot (kw,1,1,Direction.EAST);
+        
+        
+    }
+    
+}
