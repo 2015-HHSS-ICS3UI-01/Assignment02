73b933fac12f2d9a8db49bcce0d37bd15a2b9341
Peter <Peter@Katrina>
2015-09-16 10:09:01 -0400

    A2 Q1 done Q2 almost done

--- src/A2Q2.java
+++ src/A2Q2.java
@@ -1,6 +1,8 @@
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -27,5 +29,30 @@ public class A2Q2 {
         new Wall(kw, 3,2, Direction.SOUTH);
         new Wall(kw, 3,3, Direction.SOUTH);
         new Wall(kw, 3,4, Direction.SOUTH);
+        new Wall(kw, 3,5, Direction.SOUTH);
+        new Wall(kw, 3,6, Direction.SOUTH);
+        new Wall(kw, 3,7, Direction.SOUTH);
+        new Wall(kw, 3,8, Direction.SOUTH);
+        new Wall(kw, 3,9, Direction.SOUTH);
+        new Wall(kw, 3,1, Direction.EAST);
+        new Wall(kw, 3,2, Direction.EAST);
+        new Wall(kw, 3,4, Direction.EAST);
+        new Wall(kw, 3,7, Direction.EAST);
+        
+        //make robot
+        Robot karel = new Robot(kw, 3,0, Direction.EAST);
+        
+        //make thing
+        new Thing(kw, 3,9);
+        
+        //make karel 'jump the hurdles'
+        while (karel.frontIsClear()) {
+            karel.move();
+         if (karel.canPickThing()) {
+             karel.pickThing();
+         } 
+        }
+        
+        
     }
 }
