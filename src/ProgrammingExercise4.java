c34175267d9de543c6dad9c43b6cd31450f2d530
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-09-16 14:21:03 -0400

    School Change 2

---
+++ src/ProgrammingExercise4.java
@@ -0,0 +1,54 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richj0985
+ */
+public class ProgrammingExercise4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // make city for robot to live in
+        City kw = new City();
+        
+        // make robot to live in the city
+        Robot karel = new Robot(kw, 0, 0, Direction.EAST);
+        
+        // make walls within the city
+        new Wall(kw, 2, 2, Direction.NORTH);
+        new Wall(kw, 2, 3, Direction.NORTH);
+        new Wall(kw, 2, 2, Direction.WEST);
+        new Wall(kw, 3, 2, Direction.WEST);
+        new Wall(kw, 3, 2, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.EAST);
+        new Wall(kw, 2, 3, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.SOUTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.SOUTH);
+        new Wall(kw, 1, 4, Direction.WEST);
+        new Wall(kw, 4, 1, Direction.NORTH);
+        new Wall(kw, 4, 1, Direction.EAST);
+        new Wall(kw, 4, 1, Direction.SOUTH);
+        new Wall(kw, 4, 1, Direction.WEST);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 4, Direction.EAST);
+        new Wall(kw, 4, 4, Direction.SOUTH);
+        new Wall(kw, 4, 4, Direction.WEST);
+    }
+}
