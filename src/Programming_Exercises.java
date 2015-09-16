aa6bc1a01b609eee4fb790ba8ab9e1fb09e0d115
Matt <Matt@192.168.0.16>
2015-09-16 16:12:43 -0400

    added Programming_Exercises, started, added city (kw) and robot (matt, facing east, @ 1,1,) added 10 things, still trying to figure out how to get matt to stop picking things at 7 but keep moving.

--- src/Programming_Exercises.java
+++ src/Programming_Exercises.java
@@ -25,7 +25,23 @@ public class Programming_Exercises {
         City kw = new City();
         Robot matt = new Robot (kw,1,1,Direction.EAST);
         
+        new Thing (kw,1,2);
+        new Thing (kw,1,3);
+        new Thing (kw,1,4);
+        new Thing (kw,1,5);
+        new Thing (kw,1,6);
+        new Thing (kw,1,7);
+        new Thing (kw,1,8);
+        new Thing (kw,1,9);
+        new Thing (kw,1,10);
+        new Thing (kw,1,11);
         
-    }
+            if (matt.countThingsInBackpack() <=7);
+            matt.move();
+            matt.pickThing();
+            
+        
+        
+        }
     
 }
