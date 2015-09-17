6ba9792a967d633246882893d6baea6d65b6e01f
Jon <Jon@Jon-HP>
2015-09-17 17:34:41 -0400

    Home Change 4 (Quick Change)

--- src/ProgrammingExercise5.java
+++ src/ProgrammingExercise5.java
@@ -23,7 +23,7 @@ public class ProgrammingExercise5 {
         City kw = new City();
         
         // make robot to live in city
-        Robot karel = new Robot (kw, 0, 0, Direction.EAST);
+        Robot karel = new Robot (kw, 0, 2, Direction.SOUTH);
         
         // make walls within city
         new Wall(kw, 0, 0, Direction.WEST);
@@ -36,18 +36,50 @@ public class ProgrammingExercise5 {
         new Wall(kw, 7, 0, Direction.WEST);
         new Wall(kw, 8, 0, Direction.WEST);
         new Wall(kw, 9, 0, Direction.WEST);
-        new Wall(kw, 10, 0, Direction.WEST);
-        
+        new Wall(kw, 0, 1, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 2, 1, Direction.EAST);
+        new Wall(kw, 3, 1, Direction.EAST);
+        new Wall(kw, 4, 1, Direction.EAST);
+        new Wall(kw, 5, 1, Direction.EAST);
+        new Wall(kw, 6, 1, Direction.EAST);
+        new Wall(kw, 7, 1, Direction.EAST);
+        new Wall(kw, 8, 1, Direction.EAST);
+        new Wall(kw, 9, 1, Direction.EAST);
         new Wall(kw, 0, 2, Direction.EAST);
-        new Wall(kw, 1, 2, Direction.EAST);
-        new Wall(kw, 2, 2, Direction.EAST);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        new Wall(kw, 1, 6, Direction.NORTH);
+        new Wall(kw, 1, 6, Direction.EAST);
+        new Wall(kw, 2, 6, Direction.EAST);
+        new Wall(kw, 2, 6, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 3, Direction.SOUTH);
         new Wall(kw, 3, 2, Direction.EAST);
-        new Wall(kw, 4, 2, Direction.EAST);
+        new Wall(kw, 4, 3, Direction.NORTH);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.EAST);
+        new Wall(kw, 4, 5, Direction.SOUTH);
+        new Wall(kw, 4, 4, Direction.SOUTH);
+        new Wall(kw, 4, 3, Direction.SOUTH);
         new Wall(kw, 5, 2, Direction.EAST);
         new Wall(kw, 6, 2, Direction.EAST);
-        new Wall(kw, 7, 2, Direction.EAST);
-        new Wall(kw, 8, 2, Direction.EAST);
+        new Wall(kw, 7, 3, Direction.NORTH);
+        new Wall(kw, 7, 4, Direction.NORTH);
+        new Wall(kw, 7, 5, Direction.NORTH);
+        new Wall(kw, 7, 6, Direction.NORTH);
+        new Wall(kw, 7, 7, Direction.NORTH);
+        new Wall(kw, 7, 7, Direction.EAST);
+        new Wall(kw, 8, 7, Direction.EAST);
+        new Wall(kw, 8, 7, Direction.SOUTH);
+        new Wall(kw, 8, 6, Direction.SOUTH);
+        new Wall(kw, 8, 5, Direction.SOUTH);
+        new Wall(kw, 8, 4, Direction.SOUTH);
+        new Wall(kw, 8, 3, Direction.SOUTH);
         new Wall(kw, 9, 2, Direction.EAST);
-        new Wall(kw, 10, 2, Direction.EAST);
+        new Wall(kw, 9, 2, Direction.SOUTH);
     }
 }
