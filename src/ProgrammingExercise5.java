1f19d5e7ca7c146fbafc3eb03dde50d915bbd776
richj0985 <richj0985@HRH-IC0027343.wrdsb.ca>
2015-09-18 14:23:03 -0400

    School Change 4

--- src/ProgrammingExercise5.java
+++ src/ProgrammingExercise5.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -24,6 +25,7 @@ public class ProgrammingExercise5 {
         
         // make robot to live in city
         Robot karel = new Robot (kw, 0, 2, Direction.SOUTH);
+        Robot tina = new Robot (kw, 0, 2, Direction.SOUTH);
         
         // make walls within city
         new Wall(kw, 0, 0, Direction.WEST);
@@ -81,5 +83,95 @@ public class ProgrammingExercise5 {
         new Wall(kw, 8, 3, Direction.SOUTH);
         new Wall(kw, 9, 2, Direction.EAST);
         new Wall(kw, 9, 2, Direction.SOUTH);
-    }
+        
+        // make things for the robots to pick up
+        
+        new Thing(kw, 1, 2); 
+        new Thing(kw, 2, 2);
+        new Thing(kw, 4, 2);
+        new Thing(kw, 7, 2);
+        new Thing(kw, 1, 3);
+        new Thing(kw, 1, 4);
+        new Thing(kw, 1, 5);
+        new Thing(kw, 2, 4);
+        new Thing(kw, 2, 6);
+        new Thing(kw, 4, 3);
+        new Thing(kw, 4, 4);
+        new Thing(kw, 7, 3);
+        new Thing(kw, 8, 3);
+        new Thing(kw, 8, 4);
+        new Thing(kw, 8, 6);
+        new Thing(kw, 7, 7);
+        
+        new Thing(kw, 0, 3);
+        new Thing(kw, 0, 4);
+        new Thing(kw, 0, 6);
+        new Thing(kw, 1, 7);
+        new Thing(kw, 3, 6);
+        new Thing(kw, 3, 4);
+        new Thing(kw, 4, 6);
+        new Thing(kw, 4, 7);
+        new Thing(kw, 5, 7);
+        new Thing(kw, 5, 5);
+        new Thing(kw, 6, 4);
+        new Thing(kw, 6, 6);
+        new Thing(kw, 9, 3);
+        new Thing(kw, 9, 4);
+        new Thing(kw, 9, 6);
+        new Thing(kw, 0, 1);
+        new Thing(kw, 1, 0);
+        new Thing(kw, 2, 0);
+        new Thing(kw, 3, 0);
+        new Thing(kw, 4, 0);
+        new Thing(kw, 3, 1);
+        new Thing(kw, 5, 1);
+        new Thing(kw, 7, 0);
+        new Thing(kw, 7, 1);
+        new Thing(kw, 8, 1);
+        new Thing(kw, 9, 0);
+        new Thing(kw, 9, 1);
+        
+        while(true)
+        {
+            karel.turnLeft();
+            if(karel.frontIsClear())
+        }
+        
+        
+        
+        
+        
+        while(karel.getAvenue() == 2)
+        {
+            if(karel.getStreet() == 1 || karel.getStreet() == 4 || karel.getStreet() == 7)
+            {
+                if(karel.getDirection() == Direction.SOUTH)
+                {
+                    karel.turnLeft();
+                    karel.move();
+                }
+            }else
+            {
+                karel.move();
+            }
+        }
+        
+        while(karel.getAvenue() != 2)
+        {
+            if(karel.frontIsClear())
+            {
+                karel.move();
+            }else
+            {
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+            }
+        }
+        
+        while(karel.getAvenue() != 2 || karel.getStreet() == 2 || karel.getStreet() == 4 || karel.getStreet() == 8)
+        {
+            karel.turnLeft();
+        }
+   }
 }
