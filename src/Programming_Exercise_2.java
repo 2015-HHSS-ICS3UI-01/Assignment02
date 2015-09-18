0e0a5d21601ddbde61defb7561feac231178d3a5
Matt <Matt@AllegaeonV2>
2015-09-18 13:33:02 -0400

    added city, robot & walls & thing added while loop (if he cant pick thing & if the front is clear, he should move) then contingency if the front isnt clear, to go through a series of movements (hurdle), then added entire while bracket around the entire code to loop it)

--- src/Programming_Exercise_2.java
+++ src/Programming_Exercise_2.java
@@ -1,9 +1,9 @@
 
-import becker.robots.Wall;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,16 +21,41 @@ public class Programming_Exercise_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        City kw = new City();
-        Robot matt = new Robot(kw,1,1,Direction.EAST);
+        City starbucks = new City();
         
-        new Wall (kw,1,1,Direction.EAST);
-        new Wall (kw,1,2,Direction.EAST);
-        new Wall (kw,1,4,Direction.EAST);
-        new Wall (kw,1,7,Direction.EAST);
+        new Wall (starbucks,1,1,Direction.EAST);
+        new Wall (starbucks,1,2,Direction.EAST);
+        new Wall (starbucks,1,4,Direction.EAST);
+        new Wall (starbucks,1,7,Direction.EAST);
         
-        new Thing (kw,1,9);
+        new Thing (starbucks,1,9);
         
+        Robot java = new Robot(starbucks,1,1,Direction.EAST);
+        
+        while(!java.canPickThing()){
+            if (java.frontIsClear()) {
+                java.move();
+            }
+            if (!java.frontIsClear()) {
+                java.turnLeft();
+                java.move();
+                java.turnLeft();
+                java.turnLeft();
+                java.turnLeft();
+                java.move();
+                java.turnLeft();
+                java.turnLeft();
+                java.turnLeft();
+                java.move();
+                java.turnLeft();
+            }
+        }
+
+
+                
+        
+        
+                
     }
     
 }
