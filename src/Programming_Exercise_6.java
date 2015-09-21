e050a48d381e8b8fdc7223b4729909dcb2020311
Matt <Matt@AllegaeonV2>
2015-09-21 10:07:25 -0400

    -added many, many walls...taking break -added all walls & things -added robots matt & sara at (0,2) facing south

--- src/Programming_Exercise_6.java
+++ src/Programming_Exercise_6.java
@@ -2,6 +2,7 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
 import becker.robots.Wall;
 
 /*
@@ -21,6 +22,8 @@ public class Programming_Exercise_6 {
      */
     public static void main(String[] args) {
                 City kw = new City();
+                
+                //walls
         
         new Wall (kw,0,0,Direction.WEST);
         new Wall (kw,1,0,Direction.WEST);
@@ -32,7 +35,6 @@ public class Programming_Exercise_6 {
         new Wall (kw,7,0,Direction.WEST);
         new Wall (kw,8,0,Direction.WEST);
         new Wall (kw,9,0,Direction.WEST);
-        new Wall (kw,10,0,Direction.WEST);
         
         new Wall (kw,0,1,Direction.EAST);
         new Wall (kw,1,1,Direction.EAST);
@@ -44,7 +46,6 @@ public class Programming_Exercise_6 {
         new Wall (kw,7,1,Direction.EAST);
         new Wall (kw,8,1,Direction.EAST);
         new Wall (kw,9,1,Direction.EAST);
-        new Wall (kw,10,1,Direction.EAST);
         
         new Wall (kw,0,2,Direction.EAST);
         new Wall (kw,1,3,Direction.NORTH);
@@ -53,6 +54,107 @@ public class Programming_Exercise_6 {
         new Wall (kw,1,6,Direction.NORTH);
         new Wall (kw,1,6,Direction.EAST);
         new Wall (kw,2,6,Direction.EAST);
+        new Wall (kw,2,6,Direction.SOUTH);
+        new Wall (kw,2,5,Direction.SOUTH);
+        new Wall (kw,2,4,Direction.SOUTH);
+        new Wall (kw,2,3,Direction.SOUTH);
+        new Wall (kw,3,2,Direction.EAST);
+        new Wall (kw,4,3,Direction.NORTH);
+        new Wall (kw,4,4,Direction.NORTH);
+        new Wall (kw,4,5,Direction.NORTH);
+        new Wall (kw,4,5,Direction.EAST);
+        new Wall (kw,4,5,Direction.SOUTH);
+        new Wall (kw,4,4,Direction.SOUTH);
+        new Wall (kw,4,3,Direction.SOUTH);
+        new Wall (kw,5,2,Direction.EAST);
+        new Wall (kw,6,2,Direction.EAST);
+        new Wall (kw,7,3,Direction.NORTH);
+        new Wall (kw,7,4,Direction.NORTH);
+        new Wall (kw,7,5,Direction.NORTH);
+        new Wall (kw,7,6,Direction.NORTH);
+        new Wall (kw,7,7,Direction.NORTH);
+        new Wall (kw,7,7,Direction.EAST);
+        new Wall (kw,8,7,Direction.EAST);
+        new Wall (kw,8,7,Direction.SOUTH);
+        new Wall (kw,8,6,Direction.SOUTH);
+        new Wall (kw,8,5,Direction.SOUTH);
+        new Wall (kw,8,4,Direction.SOUTH);
+        new Wall (kw,8,3,Direction.SOUTH);
+        new Wall (kw,9,2,Direction.EAST);
+        new Wall (kw,9,2,Direction.SOUTH);
+        
+        //things
+        
+        new Thing (kw,0,1);
+        new Thing (kw,1,0);
+        new Thing (kw,2,0);
+        new Thing (kw,3,0);
+        new Thing (kw,3,1);
+        new Thing (kw,4,0);
+        new Thing (kw,5,1);
+        new Thing (kw,7,0);
+        new Thing (kw,7,1);
+        new Thing (kw,8,1);
+        new Thing (kw,9,0);
+        new Thing (kw,9,1);
+        
+        new Thing (kw,0,3);
+        new Thing (kw,0,4);
+        new Thing (kw,0,6);
+        new Thing (kw,1,2);
+        new Thing (kw,1,3);
+        new Thing (kw,1,4);
+        new Thing (kw,1,5);
+        
+        new Thing (kw,2,2);
+        new Thing (kw,2,4);
+        new Thing (kw,2,6);
+        new Thing (kw,2,7);
+        
+        new Thing (kw,3,6);
+        new Thing (kw,3,4);
+        
+        new Thing (kw,4,2);
+        new Thing (kw,4,3);
+        new Thing (kw,4,4);
+        new Thing (kw,4,6);
+        new Thing (kw,4,7);
+        
+        new Thing (kw,5,5);
+        new Thing (kw,5,7);
+        
+        new Thing (kw,6,4);
+        new Thing (kw,6,6);
+        
+        new Thing (kw,7,2);
+        new Thing (kw,7,3);
+        new Thing (kw,7,7);
+        
+        new Thing (kw,8,3);
+        new Thing (kw,8,4);
+        new Thing (kw,8,6);
+        
+        new Thing (kw,9,3);
+        new Thing (kw,9,4);
+        new Thing (kw,9,6);
+        
+        //robots
+        
+        Robot matt = new Robot(kw,0,2,Direction.SOUTH);
+        Robot sara = new Robot (kw,0,2,Direction.SOUTH);
+        
+        //movement
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
