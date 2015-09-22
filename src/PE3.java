f57f59710cde89696bde4c1db52b66045b6797dd
pircn0556 <pircn0556@HRH-IC0027337.wrdsb.ca>
2015-09-22 13:44:36 -0400

    Assignment two

--- src/PE3.java
+++ src/PE3.java
@@ -7,7 +7,6 @@ import becker.robots.Robot;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author pircn0556
@@ -18,11 +17,32 @@ public class PE3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Create city for robot
         City kw = new City();
-        Robot karel = new Robot(kw,5,5,Direction.EAST);
-        
-        while(karel.
-            
+
+        //Create robot for city
+        Robot karel = new Robot(kw, 5, 5, Direction.EAST);
+
+        //Algorithm for returning to 0,0
+        while (karel.getDirection() != Direction.WEST) {
+            if (true) {
+                karel.turnLeft();
+            }
+        }
+        while (karel.getAvenue() != 0) {
+            if (karel.frontIsClear()) {
+                karel.move();
+            }
+        }
+        while (karel.getDirection() != Direction.NORTH) {
+            if (true) {
+                karel.turnLeft();
+            }
+        }
+        while (karel.getStreet() != 0) {
+            if (true) {
+                karel.move();
+            }
         }
     }
-
+}
