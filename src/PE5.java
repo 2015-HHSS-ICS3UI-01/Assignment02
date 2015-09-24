816f3e870aa63b8f97308e29ff09fa4b977f95ac
pircn0556 <pircn0556@HRH-IC0027337.wrdsb.ca>
2015-09-24 14:22:02 -0400

    Assignment two

--- src/PE5.java
+++ src/PE5.java
@@ -9,7 +9,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author pircn0556
@@ -22,130 +21,132 @@ public class PE5 {
     public static void main(String[] args) {
         // Create city for robot
         City kw = new City();
-        
+
         //Create road,sidewalk,driveway
-        new Wall(kw,0,0,Direction.WEST);
-        new Wall(kw,1,0,Direction.WEST);
-        new Wall(kw,2,0,Direction.WEST);
-        new Wall(kw,3,0,Direction.WEST);
-        new Wall(kw,4,0,Direction.WEST);
-        new Wall(kw,5,0,Direction.WEST);
-        new Wall(kw,6,0,Direction.WEST);
-        new Wall(kw,7,0,Direction.WEST);
-        new Wall(kw,8,0,Direction.WEST);
-        new Wall(kw,9,0,Direction.WEST);
-        new Wall(kw,0,1,Direction.EAST);
-        new Wall(kw,1,1,Direction.EAST);
-        new Wall(kw,2,1,Direction.EAST);
-        new Wall(kw,3,1,Direction.EAST);
-        new Wall(kw,4,1,Direction.EAST);
-        new Wall(kw,5,1,Direction.EAST);
-        new Wall(kw,6,1,Direction.EAST);
-        new Wall(kw,7,1,Direction.EAST);
-        new Wall(kw,8,1,Direction.EAST);
-        new Wall(kw,9,1,Direction.EAST);
-        new Wall(kw,0,2,Direction.EAST);
-        new Wall(kw,1,3,Direction.NORTH);
-        new Wall(kw,1,4,Direction.NORTH);
-        new Wall(kw,1,5,Direction.NORTH);
-        new Wall(kw,1,6,Direction.NORTH);
-        new Wall(kw,1,6,Direction.EAST);
-        new Wall(kw,2,6,Direction.EAST);
-        new Wall(kw,2,6,Direction.SOUTH);
-        new Wall(kw,2,5,Direction.SOUTH);
-        new Wall(kw,2,4,Direction.SOUTH);
-        new Wall(kw,2,3,Direction.SOUTH);
-        new Wall(kw,3,2,Direction.EAST);
-        new Wall(kw,4,3,Direction.NORTH);
-        new Wall(kw,4,4,Direction.NORTH);
-        new Wall(kw,4,5,Direction.NORTH);
-        new Wall(kw,4,5,Direction.EAST);
-        new Wall(kw,4,5,Direction.SOUTH);
-        new Wall(kw,4,4,Direction.SOUTH);
-        new Wall(kw,4,3,Direction.SOUTH);
-        new Wall(kw,5,2,Direction.EAST);
-        new Wall(kw,6,2,Direction.EAST);
-        new Wall(kw,7,3,Direction.NORTH);
-        new Wall(kw,7,4,Direction.NORTH);
-        new Wall(kw,7,5,Direction.NORTH);
-        new Wall(kw,7,6,Direction.NORTH);       
-        new Wall(kw,8,6,Direction.SOUTH);
-        new Wall(kw,8,5,Direction.SOUTH);
-        new Wall(kw,8,4,Direction.SOUTH);
-        new Wall(kw,8,3,Direction.SOUTH);
-        new Wall(kw,9,2,Direction.EAST);
-        new Wall(kw,9,2,Direction.SOUTH);
-        new Wall(kw,8,7,Direction.SOUTH);
-        new Wall(kw,7,7,Direction.NORTH);
-        new Wall(kw,8,7,Direction.EAST);
-        new Wall(kw,7,7,Direction.EAST);
-        
+        new Wall(kw, 0, 0, Direction.WEST);
+        new Wall(kw, 1, 0, Direction.WEST);
+        new Wall(kw, 2, 0, Direction.WEST);
+        new Wall(kw, 3, 0, Direction.WEST);
+        new Wall(kw, 4, 0, Direction.WEST);
+        new Wall(kw, 5, 0, Direction.WEST);
+        new Wall(kw, 6, 0, Direction.WEST);
+        new Wall(kw, 7, 0, Direction.WEST);
+        new Wall(kw, 8, 0, Direction.WEST);
+        new Wall(kw, 9, 0, Direction.WEST);
+        new Wall(kw, 0, 1, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 2, 1, Direction.EAST);
+        new Wall(kw, 3, 1, Direction.EAST);
+        new Wall(kw, 4, 1, Direction.EAST);
+        new Wall(kw, 5, 1, Direction.EAST);
+        new Wall(kw, 6, 1, Direction.EAST);
+        new Wall(kw, 7, 1, Direction.EAST);
+        new Wall(kw, 8, 1, Direction.EAST);
+        new Wall(kw, 9, 1, Direction.EAST);
+        new Wall(kw, 0, 2, Direction.EAST);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        new Wall(kw, 1, 6, Direction.NORTH);
+        new Wall(kw, 1, 6, Direction.EAST);
+        new Wall(kw, 2, 6, Direction.EAST);
+        new Wall(kw, 2, 6, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 3, Direction.SOUTH);
+        new Wall(kw, 3, 2, Direction.EAST);
+        new Wall(kw, 4, 3, Direction.NORTH);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.EAST);
+        new Wall(kw, 4, 5, Direction.SOUTH);
+        new Wall(kw, 4, 4, Direction.SOUTH);
+        new Wall(kw, 4, 3, Direction.SOUTH);
+        new Wall(kw, 5, 2, Direction.EAST);
+        new Wall(kw, 6, 2, Direction.EAST);
+        new Wall(kw, 7, 3, Direction.NORTH);
+        new Wall(kw, 7, 4, Direction.NORTH);
+        new Wall(kw, 7, 5, Direction.NORTH);
+        new Wall(kw, 7, 6, Direction.NORTH);
+        new Wall(kw, 8, 6, Direction.SOUTH);
+        new Wall(kw, 8, 5, Direction.SOUTH);
+        new Wall(kw, 8, 4, Direction.SOUTH);
+        new Wall(kw, 8, 3, Direction.SOUTH);
+        new Wall(kw, 9, 2, Direction.EAST);
+        new Wall(kw, 9, 2, Direction.SOUTH);
+        new Wall(kw, 8, 7, Direction.SOUTH);
+        new Wall(kw, 7, 7, Direction.NORTH);
+        new Wall(kw, 8, 7, Direction.EAST);
+        new Wall(kw, 7, 7, Direction.EAST);
+
         //Snow on city
-        new Thing(kw,0,1,Direction.EAST);
-        new Thing(kw,1,0,Direction.EAST);
-        new Thing(kw,2,0,Direction.EAST);
-        new Thing(kw,3,0,Direction.EAST);
-        new Thing(kw,3,1,Direction.EAST);
-        new Thing(kw,4,0,Direction.EAST);
-        new Thing(kw,5,1,Direction.EAST);
-        new Thing(kw,7,0,Direction.EAST);
-        new Thing(kw,7,1,Direction.EAST);
-        new Thing(kw,8,1,Direction.EAST);
-        new Thing(kw,9,0,Direction.EAST);
-        new Thing(kw,9,1,Direction.EAST);
-        new Thing(kw,9,3,Direction.EAST);
-        new Thing(kw,9,4,Direction.EAST);
-        new Thing(kw,9,6,Direction.EAST);
-        new Thing(kw,8,3,Direction.EAST);
-        new Thing(kw,8,4,Direction.EAST);
-        new Thing(kw,8,6,Direction.EAST);
-        new Thing(kw,7,7,Direction.EAST);
-        new Thing(kw,7,3,Direction.EAST);
-        new Thing(kw,7,2,Direction.EAST);
-        new Thing(kw,4,2,Direction.EAST);
-        new Thing(kw,4,3,Direction.EAST);
-        new Thing(kw,4,4,Direction.EAST);
-        new Thing(kw,2,2,Direction.EAST);
-        new Thing(kw,2,4,Direction.EAST);
-        new Thing(kw,2,6,Direction.EAST);
-        new Thing(kw,1,2,Direction.EAST);
-        new Thing(kw,1,3,Direction.EAST);
-        new Thing(kw,1,4,Direction.EAST);
-        new Thing(kw,1,5,Direction.EAST);
-        new Thing(kw,0,3,Direction.EAST);
-        new Thing(kw,0,4,Direction.EAST);
-        new Thing(kw,0,6,Direction.EAST);
-        new Thing(kw,1,7,Direction.EAST);
-        new Thing(kw,3,4,Direction.EAST);
-        new Thing(kw,3,6,Direction.EAST);
-        new Thing(kw,4,6,Direction.EAST);
-        new Thing(kw,4,7,Direction.EAST);
-        new Thing(kw,5,7,Direction.EAST);
-        new Thing(kw,5,5,Direction.EAST);
-        new Thing(kw,6,4,Direction.EAST);
-        new Thing(kw,6,6,Direction.EAST);
-        
+        new Thing(kw, 0, 1, Direction.EAST);
+        new Thing(kw, 1, 0, Direction.EAST);
+        new Thing(kw, 2, 0, Direction.EAST);
+        new Thing(kw, 3, 0, Direction.EAST);
+        new Thing(kw, 3, 1, Direction.EAST);
+        new Thing(kw, 4, 0, Direction.EAST);
+        new Thing(kw, 5, 1, Direction.EAST);
+        new Thing(kw, 7, 0, Direction.EAST);
+        new Thing(kw, 7, 1, Direction.EAST);
+        new Thing(kw, 8, 1, Direction.EAST);
+        new Thing(kw, 9, 0, Direction.EAST);
+        new Thing(kw, 9, 1, Direction.EAST);
+        new Thing(kw, 9, 3, Direction.EAST);
+        new Thing(kw, 9, 4, Direction.EAST);
+        new Thing(kw, 9, 6, Direction.EAST);
+        new Thing(kw, 8, 3, Direction.EAST);
+        new Thing(kw, 8, 4, Direction.EAST);
+        new Thing(kw, 8, 6, Direction.EAST);
+        new Thing(kw, 7, 7, Direction.EAST);
+        new Thing(kw, 7, 3, Direction.EAST);
+        new Thing(kw, 7, 2, Direction.EAST);
+        new Thing(kw, 4, 2, Direction.EAST);
+        new Thing(kw, 4, 3, Direction.EAST);
+        new Thing(kw, 4, 4, Direction.EAST);
+        new Thing(kw, 2, 2, Direction.EAST);
+        new Thing(kw, 2, 4, Direction.EAST);
+        new Thing(kw, 2, 6, Direction.EAST);
+        new Thing(kw, 1, 2, Direction.EAST);
+        new Thing(kw, 1, 3, Direction.EAST);
+        new Thing(kw, 1, 4, Direction.EAST);
+        new Thing(kw, 1, 5, Direction.EAST);
+        new Thing(kw, 0, 3, Direction.EAST);
+        new Thing(kw, 0, 4, Direction.EAST);
+        new Thing(kw, 0, 6, Direction.EAST);
+        new Thing(kw, 1, 7, Direction.EAST);
+        new Thing(kw, 3, 4, Direction.EAST);
+        new Thing(kw, 3, 6, Direction.EAST);
+        new Thing(kw, 4, 6, Direction.EAST);
+        new Thing(kw, 4, 7, Direction.EAST);
+        new Thing(kw, 5, 7, Direction.EAST);
+        new Thing(kw, 5, 5, Direction.EAST);
+        new Thing(kw, 6, 4, Direction.EAST);
+        new Thing(kw, 6, 6, Direction.EAST);
+
         //Make ShovelBot robots(karel and tina)
-        Robot karel = new Robot(kw,0,2,Direction.SOUTH);
-        Robot tina = new Robot(kw,0,2,Direction.SOUTH);
+        Robot karel = new Robot(kw, 0, 2, Direction.SOUTH);
+        Robot tina = new Robot(kw, 0, 2, Direction.SOUTH);
+
+        //Make robots pick up the snow 
+        while(true){
+            karel.turnLeft();
+        if (karel.frontIsClear()) {
+            while (karel.frontIsClear()) {
+                if (karel.canPickThing()) {
+                    karel.pickThing();
+                } else {
+                    karel.move();
+                }
+
+            }
+        }else{
+            karel.turnLeft();
+            karel.turnLeft();
+            karel.turnLeft();
+            karel.move();
+        }
+        }
         
-        //Make robots pick up the snow
-        while(karel.frontIsClear()){
-            if(karel.canPickThing()){ 
-                karel.pickThing();
-                karel.move();
-            }else{
-                karel.move();
-                karel.turnLeft();
-            }
-            if(karel.frontIsClear()){
-                karel.move();
-            }else{
-                karel.turnLeft();
-                karel.turnLeft();
-                karel.turnLeft();
-            }
-            
-        } 
     }
 }
