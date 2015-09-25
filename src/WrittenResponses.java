368421f49925186dccf6dc7152b291936e68dee7
moore3607 <moore3607@HRH-IC0027344.wrdsb.ca>
2015-09-25 12:53:10 -0400

    added the written responses

---
+++ src/WrittenResponses.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moore3607
+ */
+public class WrittenResponses {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //1a. true
+        //1b. true
+        //1c. false
+        //1d. false
+        //1e. false
+        //1f. true
+        //1g. false
+        //1h. true
+        
+        //2. no they do not. in the first one, karel will only turn left
+        //if he can pick something up and turn right if he cant. the second
+        //one is the exact opposite
+        
+        
+        
+    }
+}
